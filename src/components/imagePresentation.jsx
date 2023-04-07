ab0e178b7aa4c1c83ff7b2c4d635ae6a8ccbe38f
Diego Gonzalez <gonzalezbusnego@gmail.com>
2023-04-07 11:44:57 -0400

    Add country Search Presentation that i mistake

--- src/components/imagePresentation.jsx
+++ src/components/imagePresentation.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './imgGalery.css'
-import { hotely, cloudCreate, gatoCraft, owlShop, ticTacToe } from '../content'
+import {
+  hotely,
+  cloudCreate,
+  gatoCraft,
+  owlShop,
+  ticTacToe,
+  countrySearch
+} from '../content'
 
 export function Presentations() {
   return (
@@ -9,6 +16,7 @@ export function Presentations() {
       <img loading='lazy' src={cloudCreate.presentation} />
       <img loading='lazy' src={ticTacToe.presentation} />
       <img loading='lazy' src={gatoCraft.presentation} />
+      <img loading='lazy' src={countrySearch.presentation} />
       <img loading='lazy' src={owlShop.presentation} />
     </figure>
   )
